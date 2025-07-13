80a87538da0482fc1854d699277c83209672637b
Aniket20281 <aniket20281@iiitd.ac.in>
2025-07-13 11:15:27 +0530

    fixed hairnet alerts & added image viewing feature

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,16 @@ app.use(cors());
 app.use('/api/alerts', alertRoutes); // Use the router
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((err) => console.error('MongoDB connection error:', err));
+
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: 'guardex', 
+})
+.then(() => console.log('Connected to MongoDB - guardex'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
 
 // Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
